75d9a606f2acd8424f76cf30768ce2280af33854
aubymori <aubyomori@gmail.com>
2024-01-13 21:57:43 -0600

    [CTestWindow] Actually this should probably go before

--- testwindow/TestWindow.h
+++ testwindow/TestWindow.h
@@ -5,12 +5,12 @@
 class CTestWindow : public ATL::CWindowImpl<CTestWindow>
 {
 public:
+	DECLARE_WND_CLASS(L"KappaBar_TestWindow")
+
 	BEGIN_MSG_MAP(CTestWindow)
 		MESSAGE_HANDLER(WM_PAINT, OnPaint)
 		MESSAGE_HANDLER(WM_DESTROY, OnDestroy)
 	END_MSG_MAP()
-
-	DECLARE_WND_CLASS(L"KappaBar_TestWindow")
 
 	LRESULT OnPaint(UINT uMsg, WPARAM wParam, LPARAM lParam, BOOL &bHandled);
 	LRESULT OnDestroy(UINT uMsg, WPARAM wParam, LPARAM lParam, BOOL &bHandled);
